bf3b33957fba5ec1832fa30d97fe24f7a7a7df5c
Eddy Lane <Naedin@gmail.com>
2014-06-13 14:51:09 +0100

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cloudcatcherDesktopApp
+ * @description
+ * # cloudcatcherDesktopApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cloudcatcherDesktopApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngSanitize'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cloudcatcherDesktopApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cloudcatcherDesktopApp
+ */
+angular.module('cloudcatcherDesktopApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
