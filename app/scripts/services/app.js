da509a7b645c1d6b187627519eecdedf8cda1d37
Eddy Lane <eddy@uvd.co.uk>
2014-07-28 14:26:35 +0100

    Changed CLOUDCATCHER_ENDPOINT to https

--- app/scripts/services/app.js
+++ app/scripts/services/app.js
@@ -15,7 +15,7 @@ angular
         'xml'
     ])
     .constant('ITUNES_URL', 'https://itunes.apple.com/')
-    .constant('CLOUDCATCHER_URL', 'http://arcane-river-7182.herokuapp.com/api/v1/')
+    .constant('CLOUDCATCHER_URL', 'https://arcane-river-7182.herokuapp.com/api/v1/')
     .constant('GOOGLE_FEED_URL', '//ajax.googleapis.com/ajax/services/feed/')
     .constant('FIREBASE_URL', 'https://podcatcher.firebaseio.com')
 ;
