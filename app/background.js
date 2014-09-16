2a6fa2386fd342329a0303abaf04fec6e8df3994
Eddy Lane <Naedin@gmail.com>
2014-09-16 23:42:32 +0100

    Updated notifications to use locally stored image by default

--- app/background.js
+++ app/background.js
@@ -10,13 +10,21 @@ chrome.app.runtime.onLaunched.addListener(function() {
 chrome.gcm.onMessage.addListener(function(message) {
     console.log('message', message);
 
-    chrome.notifications.create('cloudcatcher' + Math.random(), {
-        type: 'basic',
-        message: 'Whats Occuring?',
-        title: 'Yo Dawg',
-        iconUrl: 'calculator-128.png'
-    }, function (notificationId) {
-        console.log('done notification', notificationId);
+    chrome.storage.local.get(message.data.slug + ':image', function (data) {
+
+        console.log('imagedata', data);
+
+        chrome.notifications.create('cloudcatcher' + Math.random(), {
+            type: 'basic',
+            message: message.data.title,
+            eventTime: parseInt(message.data.date, 10),
+            title: message.data.podcast,
+            iconUrl: data[message.data.slug + ':image'] || 'calculator-128.png'
+        }, function (notificationId) {
+            console.log('done notification', notificationId);
+        });
+
+
     });
 
 });
